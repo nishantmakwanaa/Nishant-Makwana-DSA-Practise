1defd9a1e8dfdc2e3c3e95b61a675b4af5b216c4
Nishant Makwana <144738854+nishantmakwanaa@users.noreply.github.com>
2024-02-12 19:33:50 +0530

    Reverse Integer - LeetCode
    DSA QUSTIONS

---
+++ ReverseInteger.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+#include<climits>
+
+using namespace std;
+
+int main() {
+
+int x;
+
+cout << "ENTER THE NUMBER : ";
+
+cin >> x;
+
+int ans=0;
+
+while(x != 0) {
+
+int digit = x % 10;
+
+if((ans > INT_MAX/10) || (ans < INT_MIN/10)) {
+
+return 0;
+
+}
+
+ans = (ans * 10) + digit;
+
+x = x / 10;
+
+}
+
+cout << "REVERSE INTEGER IS : " << ans;
+
+}
